d4a33e8ca8e3c610a4794092c0a6c30e0e00562e
Paul Quintero <paul.alonzoqr@gmail.com>
2022-05-03 21:04:43 -0600

    Change image to svg

--- README.md
+++ README.md
@@ -1,16 +1,98 @@
+
 # logo
+
+  
 
 **To use the script only import inside tag HEAD**
 
+  
 `<script src="logotooltip.js"></script>`
 
 **modify the next properties if you need to change de functionality**
 
-> Text to display in the Tooltip
-`textTooltip = "'Powered by wiringbits'";`
+  
 
-> Page to redirect
-`hrefLogo = "https://wiringbits.net/?utm_medium=referrer&utm_source=${window.location.hostname}";`
+     /**
+      * Text to display in the Tooltip
+      */
+    `textTooltip = "'Powered by wiringbits'";`
 
-> ref to the logo image (i recommend you use a image transparent)
-`srcImage = "https://raw.githubusercontent.com/paulquintero/logo/75b9d2ccb0c2d743504448224c68cc126f2acb1b/logo.png";`
+
+       /**
+        * Page to redirect
+        */
+        hrefLogo = "https://wiringbits.net/?utm_medium=referrer&utm_source=${window.location.hostname}";
+
+      /**
+        * Logo rendered in SVG, change this to change the form logo
+       */
+    
+        `svgLogo = "M0,10.5982,10.5982,0l2.9874,2.9874L2.9874,13.5856Zm44.6556,2.9874,2.9873-2.9874L37.0447,0,34.0573,2.9874ZM16.21,26.8091l7.6108-7.6108,7.6108,7.6108L42.03,16.2109,31.432,5.6127l-7.6108,7.61L16.21,5.6127,5.6122,16.2109Zm0-15.2217,4.6234,4.6235L16.21,20.8338l-4.623-4.6229Zm15.2216,0,4.6235,4.6235L31.432,20.8338l-4.6234-4.6229Z";`
+
+     Color's logo, change this to change the whole color
+    
+    `svgLogoColor = "#39f`
+
+>There are a bunch of variables that you can modify, be careful these all variables can affects the styles and the display
+
+     /**
+    * Color of the text to show when get hover inside the logo
+    */
+    bakgroundTooltip = "#73d8ff";
+    
+     /**
+    * Color of the text to show when get hover inside the logo
+    */
+    colorTextTooltip = "#000000";
+    
+     /**
+    * Class name to define the main container 
+    */
+    mainContainer = `wiringbitsLogo`;
+    
+    /**
+    * Class name to set the container 
+    */
+    fixed = "wiringbitsFixed";
+    
+    /**
+    * Class name to define the position
+    */
+    floatLogo = "wiringbitsFloatLogo";
+    
+    /**
+    * Class name to define shine
+    */
+    highLight ="wiringbitsHighlight";
+    
+    /**
+    * Class name to define links styles
+    */
+    link = "wiringbitsLogoLink";
+    
+    /**
+    * Background style, currently using a gradient style
+    */
+    backgroundStyle= "background-image: linear-gradient(to bottom right, #60d3ff, #ffffff);";
+    
+    /**
+    * Distance from logobottom
+    */
+    alignLogoBottom = "5px";
+        
+    /**
+    * Distance from logo right
+    */
+    alignLogoRight = "5px";
+    
+    /**
+    * Width Logo
+    */
+    width = "50px;";
+    
+    /**
+    * Height Logo
+    */
+    height = "50px";
+    
+    borderRadius = "8px";
--- logotooltip.js
+++ logotooltip.js
@@ -5,7 +5,8 @@ document.addEventListener("DOMContentLoaded", function (event) {
 class LogoToolTip {
     textTooltip = "'Powered by wiringbits'";
     hrefLogo = `https://wiringbits.net/?utm_medium=referrer&utm_source=${window.location.hostname}`;
-    srcImage = "https://raw.githubusercontent.com/paulquintero/logo/75b9d2ccb0c2d743504448224c68cc126f2acb1b/logo.png";
+    svgLogo = "M0,10.5982,10.5982,0l2.9874,2.9874L2.9874,13.5856Zm44.6556,2.9874,2.9873-2.9874L37.0447,0,34.0573,2.9874ZM16.21,26.8091l7.6108-7.6108,7.6108,7.6108L42.03,16.2109,31.432,5.6127l-7.6108,7.61L16.21,5.6127,5.6122,16.2109Zm0-15.2217,4.6234,4.6235L16.21,20.8338l-4.623-4.6229Zm15.2216,0,4.6235,4.6235L31.432,20.8338l-4.6234-4.6229Z";
+    svgLogoColor = "#39f";
 
     bakgroundTooltip = "#73d8ff";
     colorTextTooltip = "#000000";
@@ -14,7 +15,7 @@ class LogoToolTip {
     floatLogo = "wiringbitsFloatLogo";
     highLight ="wiringbitsHighlight";
     link = "wiringbitsLogoLink";
-    backGroundStyle = "background-image: linear-gradient(to bottom right, #60d3ff, #ffffff);";
+    backgroundStyle = "background-image: linear-gradient(to bottom right, #60d3ff, #ffffff);";
     alignLogoBottom = "5px";
     alignLogoRight = "5px";
     width = "50px;";
@@ -23,7 +24,7 @@ class LogoToolTip {
     constructor() {
 
     }
-    
+
     styleMainContainer() {
         var style = document.createElement("style");
         style.innerHTML = `.${this.mainContainer} {width: ${this.width};  height: ${this.height};}
@@ -43,8 +44,8 @@ class LogoToolTip {
     styleFloatLogo() {
         let style = document.createElement("style");
         style.innerHTML =
-            `.${this.floatLogo} {  background-color: #fff;  border-radius: 34px;  border: 3px solid #3f3f3f;  ${this.backGroundStyle}  padding: 2px;}
-            .${this.floatLogo} img {  width: ${this.width};  height: ${this.height};  object-fit: contain;}`;
+            `.${this.floatLogo} {  background-color: #fff;  border-radius: 34px;  border: 3px solid #3f3f3f;  ${this.backgroundStyle}  padding: 2px;}
+            .${this.floatLogo} svg {  width: ${this.width};  height: ${this.height}; }`;
         document.getElementsByTagName('head')[0].appendChild(style);
         return style;
     }
@@ -66,11 +67,20 @@ class LogoToolTip {
         const container = document.createElement("div");
         const logo = document.createElement("div");
         const link = document.createElement("a");
-        const img = document.createElement("img");
-
+        const svg =  document.createElementNS("http://www.w3.org/2000/svg", "svg");
+        const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
         link.setAttribute("href", this.hrefLogo);
         link.setAttribute("target", "_Blank");
-        img.setAttribute("src", this.srcImage);
+        
+        svg.setAttribute("viewBox", "0 0 47.6429 26.8091");
+        svg.setAttribute("data-name", "Layer 1");
+        svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+        svg.setAttribute("id", "Layer_1");
+        path.setAttribute("d", this.svgLogo);
+        path.setAttribute("style", `fill: ${this.svgLogoColor}`);
+        path.setAttribute("id","logoMark");
+
+        
 
         this.styleMainContainer();
         this.styleFixed();
@@ -83,8 +93,10 @@ class LogoToolTip {
         link.className = this.link;
         container.appendChild(logo);
         logo.appendChild(link);
-        link.appendChild(img);
+        svg.appendChild(path);
+        link.appendChild(svg);
         document.body.appendChild(container);
+        
     }
 }
 
